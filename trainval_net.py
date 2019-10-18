b10836aee5091cc3411af9a0b856d3279bc9c5e4
zhuriheng <zrh0712@outlook.com>
2019-10-18 23:47:48 +0800

    fix bug of max_epoch

--- trainval_net.py
+++ trainval_net.py
@@ -349,7 +349,7 @@ if __name__ == '__main__':
 
     iters_per_epoch = int(train_size / args.batch_size)
 
-    for epoch in range(args.start_epoch, args.max_epochs):
+    for epoch in range(args.start_epoch, args.max_epochs + 1):
         # setting to train mode
         FPN.train()
         loss_temp = 0
